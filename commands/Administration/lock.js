4df483ef1fa7860923380e9516840b3ee272c117
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-02-06 16:58:54 +0100

    ➕ New: lock and unlock commands

---
+++ commands/Administration/lock.js
@@ -0,0 +1,26 @@
+module.exports.run = async (client, message, args) => {
+    if(!message.guild.me.permissionsIn(message.channel).has("MANAGE_CHANNELS")) return message.channel.send('⚠️ Je n\'ai pas les permissions de gérer ce salon.');
+
+    await message.channel.overwritePermissions([
+        {
+            id: message.guild.roles.everyone.id,
+            deny: ["SEND_MESSAGES", "ADD_REACTIONS"]
+        }
+    ]).then(() => {
+        message.channel.send(`✅ Le salon ${message.channel} a bien été fermé.`);  
+    }).catch(err => {
+        message.channel.send('Une erreur est survenue, veuillez réessayer. \n```' + err + '\n```');
+    });
+}
+
+module.exports.help = {
+    name: "lock",
+    aliases: ["lock"],
+    category: 'Administration',
+    description: "Fermer un salon",
+    usage: "",
+    cooldown: 5,
+    memberPerms: ["MANAGE_CHANNELS"],
+    botPerms: ["MANAGES_CHANNELS"],
+    args: false
+}
---
+++ commands/Administration/unlock.js
@@ -0,0 +1,26 @@
+module.exports.run = async (client, message, args) => {
+    if(!message.guild.me.permissionsIn(message.channel).has("MANAGE_CHANNELS")) return message.channel.send('⚠️ Je n\'ai pas les permissions de gérer ce salon.');
+
+    await message.channel.overwritePermissions([
+        {
+            id: message.guild.roles.everyone.id,
+            allow: ["SEND_MESSAGES"]
+        }
+    ]).then(() => {
+        message.channel.send(`✅ Le salon ${message.channel} a bien été réouvert.`);  
+    }).catch(err => {
+        message.channel.send('Une erreur est survenue, veuillez réessayer. \n```' + err + '\n```');
+    });
+}
+
+module.exports.help = {
+    name: "unlock",
+    aliases: ["unlock"],
+    category: 'Administration',
+    description: "Rouvrir un salon",
+    usage: "",
+    cooldown: 5,
+    memberPerms: ["MANAGE_CHANNELS"],
+    botPerms: ["MANAGES_CHANNELS"],
+    args: false
+}
