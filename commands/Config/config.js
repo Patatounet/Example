ce77fd641747e287413cdd92b37f1594224d4be9
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-02-14 17:13:29 +0100

    🆙 Now show if channel is deleted

--- commands/Config/config.js
+++ commands/Config/config.js
@@ -7,12 +7,18 @@ module.exports.run = (client, message, args, data) => {
         .setDescription('Configuration actuelle du serveur ' + message.guild.name)
         .addField('🌐 Général', `Préfixe: \`${data.prefix}\``)
         .addField('🛡️ Protection', `Raidmode: \`${data.plugins.protection.raidmode ? "Activé" : "Désactivé"}\` \nAnti-give-role: \`${data.plugins.protection.antigiverole ? "Activé" : "Désactivé"}\` \nAntiban: \`${data.plugins.protection.antiban ? "Activé" : "Désactivé"}\` \nAntilien: \`${data.plugins.protection.antilink ? "Activé" : "Désactivé"}\``)
-        .addField('👋 Message de bienvenue', `Activé: \`${data.plugins.welcome.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.welcome.message}\` \nSalon: ${data.plugins.welcome.channel ? '<#' + data.plugins.welcome.channel + '>' : "`MP`"}`)
-        .addField('💔 Message d\'aurevoir', `Activé: \`${data.plugins.goodbye.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.goodbye.message}\` \nSalon: ${data.plugins.goodbye.channel ? '<#' + data.plugins.goodbye.channel + '>' : "`MP`"}`)
-        .addField('⚒️ Modération', `Activé: ${data.plugins.logs.enabled ? "`Oui`" : "`Non`"} \nSalon de logs: ${data.plugins.logs.channel ? '<#' + data.plugins.logs.channel + '>' : "`Aucun`"}`)
-        .addField('💡 Suggestions', `Activé: ${data.plugins.suggestion.enabled ? "`Oui`" : "`Non`"} \nSalon: ${data.plugins.suggestion.channel ? '<#' + data.plugins.suggestion.channel + '>' : "`Aucun`"}`)
+        .addField('👋 Message de bienvenue', `Activé: \`${data.plugins.welcome.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.welcome.message}\` \nSalon: ${data.plugins.welcome.channel ? checkDeleted("welcome") : "`MP`"}`)
+        .addField('💔 Message d\'aurevoir', `Activé: \`${data.plugins.goodbye.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.goodbye.message}\` \nSalon: ${data.plugins.goodbye.channel ? checkDeleted("goodbye") : "`MP`"}`)
+        .addField('⚒️ Modération', `Activé: ${data.plugins.logs.enabled ? "`Oui`" : "`Non`"} \nSalon de logs: ${data.plugins.logs.channel ? checkDeleted("logs") : "`Aucun`"}`)
+        .addField('💡 Suggestions', `Activé: ${data.plugins.suggestion.enabled ? "`Oui`" : "`Non`"} \nSalon: ${data.plugins.suggestion.channel ? checkDeleted("suggestion") : "`Aucun`"}`)
         .setFooter(client.config.embed.footer, client.user.displayAvatarURL());
     message.channel.send(embed);
+
+    function checkDeleted(plugin) {
+        const channel = client.channels.cache.get(data.plugins[plugin].channel);
+        if(!channel) return "**Salon supprimé**";
+        else return `<#${channel.id}>`;
+    }
 }
 
 module.exports.help = {
