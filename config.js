720ff4252a78da20de243c859fa9a15ddc637f3b
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-02-15 18:42:27 +0100

    📝 Update status

--- config.js
+++ config.js
@@ -18,14 +18,8 @@ module.exports = {
         id: "383667743037194240",
         name: "COCO150#0001"
     },
-    status: [
-        {
-            name: "{serversCount} serveurs et {usersCount} utilisateurs",
-            type: "COMPETING"
-        },
-        {
-            name: "@RainsBot pour avoir de l'aide!",
-            type: "PLAYING"
-        }
-    ]
+    status: {
+        name: "{serversCount} servers | {usersCount} users",
+        type: "COMPETING"
+    }
 }
