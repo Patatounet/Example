99601eb22308bd94e25b85a6db7532ec3d23bf91
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-14 19:46:54 +0100

    🎉 V2.0.0

--- commands/Moderation/unban.js
+++ commands/Moderation/unban.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports.run = async (client, message, args, data) => {
     const user = args[0];
     if(!user || isNaN(user)) return message.channel.send(`⚠️ Veuillez renseigner l'id d'un membre.`);
@@ -13,8 +15,24 @@ module.exports.run = async (client, message, args, data) => {
 
         if(!isBanned) return message.channel.send(`⚠️ Cet utilisateur n'est pas banni.`);
 
-        message.guild.members.unban(toUnban, reason).catch(() => {});
-        message.channel.send(`✅ ${toUnban} s'est fait débannir pour la raison suivante: **${reason}**`);
+        message.guild.members.unban(toUnban, reason).then(() => {
+            toUnban.send(`Vous avez été unban du serveur **${message.guild.name}** par ${message.author}. Raison : **${reason}**`).catch(() => {});
+
+            message.channel.send(`✅ ${toUnban} s'est fait débannir pour la raison suivante: **${reason}**`);
+
+            if(data.plugins.logs.enabled) {
+                if(message.guild.channels.cache.get(data.plugins.logs.channel)) {
+                    const embed = new MessageEmbed()
+                        .setColor('GREEN')
+                        .setDescription(`L'utilisateur **${toUnban.username}** s'est fait débannir par ${message.author}. \nRaison: **${reason}**`)
+                        .setFooter(client.config.embed.footer, client.user.displayAvatarURL());
+                    message.guild.channels.cache.get(data.plugins.logs.channel).send(embed);
+                }
+            }
+        }).catch(err => {
+            message.channel.send(`Une erreur est survenue, veuillez réessayer. \n\`\`\`js\n${err.stack}\n\`\`\``);
+            console.error(err);
+        });
     }
 }
 
