b4dc8a97ae342c29c76aa5d11417305787196644
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-14 19:47:17 +0100

    🎉 V2.0.0

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client, message, args, data) => {
-	const user = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(u => u.username.toLowerCase().includes(args[0].toLowerCase()));
+    const user = message.mentions.users.first() || client.users.cache.get(args[0]) || client.users.cache.find(u => u.username.toLowerCase().includes(args[0].toLowerCase()));
     const muteRole = message.guild.roles.cache.find(r => r.id == data.muterole);
     const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée');
 
@@ -13,22 +13,31 @@ module.exports.run = async (client, message, args, data) => {
 
     if(muteRole.position >= message.guild.me.roles.highest.position) return message.channel.send(`⚠️ Impossible de unmute ce membre, le rôle \`${muteRole}\` est plus haut que mon rôle le plus haut!`);
 
-    await member.roles.remove(muteRole.id).catch(err => {
+    await member.roles.remove(muteRole.id).then(async () => {
+        user.send(`Vous avez été unmute du serveur **${message.guild.name}** par ${message.author}. Raison : ${reason}`);
+        message.channel.send(`✅ ${user} s'est fait unmute par ${message.author} pour la raison suivante: **${reason}**`);
+
+        const dbUser = await client.findOrCreateUser(user);
+        if(dbUser.tempmutes.some(mute => mute.guildID === message.guild.id)) {
+            dbUser.tempmutes = dbUser.tempmutes.filter(mute => mute.guildID !== message.guild.id);
+
+            dbUser.markModified("tempmutes");
+            dbUser.save();
+        }
+
+        if(data.plugins.logs.enabled) {
+            if(message.guild.channels.cache.get(data.plugins.logs.channel)) {
+                const embed = new MessageEmbed()
+                    .setColor('ORANGE')
+                    .setDescription(`L'utilisateur **${user.username}** s'est fait unmute par ${message.author}. \nRaison: **${reason}**`)
+                    .setFooter(client.config.embed.footer, client.user.displayAvatarURL());
+                message.guild.channels.cache.get(data.plugins.logs.channel).send(embed);
+            }
+        }
+    }).catch(err => {
         console.log(err);
         message.channel.send(`Une erreur est survenue, veuillez réessayer. \n\`\`\`js\n${err}\n\`\`\``);
     });
-
-    message.channel.send(`✅ ${user} s'est fait unmute par ${message.author} pour la raison suivante: **${reason}**`);
-
-    if(data.plugins.logs.enabled) {
-        if(data.plugins.logs.channel) {
-            const embed = new MessageEmbed()
-                .setColor('ORANGE')
-                .setDescription(`L'utilisateur **${user.username}** s'est fait unmute par ${message.author}. \nRaison: **${reason}**`)
-                .setFooter(client.config.embed.footer, client.user.displayAvatarURL());
-            message.guild.channels.cache.get(data.plugins.logs.channel).send(embed);
-        }
-    }
 }
 
 module.exports.help = {
