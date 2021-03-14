648f619af29c72fc702b93c5dc76ec2686cae89c
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-14 19:52:44 +0100

    🎉 V2.0.0

--- commands/Config/config.js
+++ commands/Config/config.js
@@ -1,18 +1,65 @@
-const { MessageEmbed } = require('discord.js');
-
 module.exports.run = (client, message, args, data) => {
-    const embed = new MessageEmbed()
-        .setColor(client.config.embed.color)
-        .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
-        .setDescription('Configuration actuelle du serveur ' + message.guild.name)
-        .addField('🌐 Général', `Préfixe: \`${data.prefix}\``)
-        .addField('🛡️ Protection', `Raidmode: \`${data.plugins.protection.raidmode ? "Activé" : "Désactivé"}\` \nAnti-give-role: \`${data.plugins.protection.antigiverole ? "Activé" : "Désactivé"}\` \nAntiban: \`${data.plugins.protection.antiban ? "Activé" : "Désactivé"}\` \nAntilien: \`${data.plugins.protection.antilink ? "Activé" : "Désactivé"}\``)
-        .addField('👋 Message de bienvenue', `Activé: \`${data.plugins.welcome.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.welcome.message}\` \nSalon: ${data.plugins.welcome.channel ? checkDeleted("welcome") : "`MP`"}`)
-        .addField('💔 Message d\'aurevoir', `Activé: \`${data.plugins.goodbye.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.goodbye.message}\` \nSalon: ${data.plugins.goodbye.channel ? checkDeleted("goodbye") : "`MP`"}`)
-        .addField('⚒️ Modération', `Activé: ${data.plugins.logs.enabled ? "`Oui`" : "`Non`"} \nSalon de logs: ${data.plugins.logs.channel ? checkDeleted("logs") : "`Aucun`"}`)
-        .addField('💡 Suggestions', `Activé: ${data.plugins.suggestion.enabled ? "`Oui`" : "`Non`"} \nSalon: ${data.plugins.suggestion.channel ? checkDeleted("suggestion") : "`Aucun`"}`)
-        .setFooter(client.config.embed.footer, client.user.displayAvatarURL());
-    message.channel.send(embed);
+    message.channel.send({ 
+        embed: {
+            color: client.config.embed.color,
+            author: {
+                icon_url: message.guild.iconURL({ dynamic: true }),
+                name: message.guild.name
+            },
+            description: `**Configuration actuelle du serveur ${message.guild.name}** \nSi vous souhaitez activer ou désactiver certaines plugins, faites \`${data.prefix}enable <plugin>\`. Pour plus d'informations, faites \`${data.prefix}help\``,
+            fields: [
+                {
+                    name: "🌐 Général",
+                    value: `Préfixe: \`${data.prefix}\` \nLangue: \`${data.lang}\``,
+                    inline: false
+                },
+                {
+                    name: "👋 Message de bienvenue",
+                    value: `Activé: \`${data.plugins.welcome.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.welcome.message}\` \nSalon: ${data.plugins.welcome.channel ? checkDeleted("welcome") : "`MP`"}`,
+                    inline: true
+                },
+                {
+                    name: "💔 Message d\'aurevoir",
+                    value: `Activé: \`${data.plugins.goodbye.enabled ? "Oui" : "Non"}\` \nMessage: \`${data.plugins.goodbye.message}\` \nSalon: ${data.plugins.goodbye.channel ? checkDeleted("goodbye") : "`MP`"}`,
+                    inline: false
+                },
+                {
+                    name: "🥇 Levels",
+                    value: `Activé: ${data.plugins.levels.enabled ? "`Oui`" : "`Non`"} \nSalon de montées en niveaux: ${data.plugins.levels.level_up_channel ? `<#${data.plugins.levels.level_up_channel}>` : "`Aucun`"}`,
+                    inline: true
+                },
+                {
+                    name: "💵 Économie",
+                    value: `Activée sur le serveur: ${data.plugins.economy.enabled ? '`Oui`' : '`Non`'} \nDevise: \`${data.plugins.economy.currency}\``,
+                    inline: true
+                },
+                {
+                    name: "💡 Suggestions",
+                    value: `Activé: ${data.plugins.suggestion.enabled ? "`Oui`" : "`Non`"} \nSalon: ${data.plugins.suggestion.channel ? checkDeleted("suggestion") : "`Aucun`"}`,
+                    inline: true
+                },
+                {
+                    name: "🛡️ Protection",
+                    value: `Raidmode: \`${data.plugins.protection.raidmode ? "Activé" : "Désactivé"}\` \nAnti-give-role: \`${data.plugins.protection.antigiverole ? "Activé" : "Désactivé"}\` \nAntiban: \`${data.plugins.protection.antiban ? "Activé" : "Désactivé"}\` \nAntilien: \`${data.plugins.protection.antilink ? "Activé" : "Désactivé"}\` \nAntimaj: \`${data.plugins.protection.antimaj ? "Activé" : "Désactivé"}\` \n**Antispam**: \nActivé: \`${data.plugins.protection.antispam?.enabled ? "Activé" : "Désactivé"}\` \nSalon(s) ignoré(s): ${data.plugins.protection.antispam?.ignored_channels?.length >= 1 ? data.plugins.protection.antispam.ignored_channels.map(c => `<#${c}>`).join(", ") : "`Aucun`"}`,
+                    inline: true
+                },
+                {
+                    name: "⚒️ Modération",
+                    value: `Activé: ${data.plugins.logs.enabled ? "`Oui`" : "`Non`"} \nSalon de logs: ${data.plugins.logs.channel ? checkDeleted("logs") : "`Aucun`"}`,
+                    inline: true
+                },
+                {
+                    name: "\u200b",
+                    value: "\u200b",
+                    inline: true
+                }
+            ],
+            footer: {
+                icon_url: client.user.displayAvatarURL(),
+                text: client.config.embed.footer
+            }
+        } 
+    });
 
     function checkDeleted(plugin) {
         const channel = client.channels.cache.get(data.plugins[plugin].channel);
