0dbcf53fd4c43efd3a5a067b364eeb92bbc1a473
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-14 19:53:03 +0100

    Update enable.js

--- commands/Config/enable.js
+++ commands/Config/enable.js
@@ -1,113 +1,111 @@
 module.exports.run = async (client, message, args, data) => {
     let plugin = args[0];
-    if(!plugin) return message.channel.send('⚠️ Ce plugin n\'existe pas. Voici la liste des plugins: `welcome`, `goodbye`, `logs`, `autorole`, `suggestion`. \nVous ne trouvez pas ce que vous voulez ? Faites `' + data.prefix + 'config` pour voir les autres configurations. Le module de protection s\'active séparement via les commandes `raidmode`, `antigiverole`, `antiban`, `antilink`.');
 
-    switch (plugin.toLowerCase()) {
+    switch (plugin?.toLowerCase()) {
         case "protection": {
             message.channel.send('ℹ️ Le plugin protection n\'est pas activable. Vous pouvez activer le raidmode, l\'antigiverole, l\'antilien ou l\'antiban séparément via leur commande respective (ex: `' + data.prefix + 'raidmode`)');
             break;
         }
         case "welcome": {
-            if(data.plugins.welcome.enabled) {
-                data.plugins.welcome.enabled = false;
-
-                data.markModified("plugins.welcome.enabled");
-                data.save();
-
-                message.channel.send('✅ Le plugin `welcome` a bien été **désactivé**.')
-            } else {
+            if(!data.plugins.welcome.enabled) {
                 data.plugins.welcome.enabled = true;
 
                 data.markModified("plugins.welcome.enabled");
                 data.save();
 
-                message.channel.send('✅ Le plugin `welcome` a bien été **activé**. Faites `' + data.prefix + 'welcome <message | channel | test>` pour modifier le message et le salon de bienvenue!');
+                message.channel.send(`✅ Le plugin \`welcome\` a bien été **activé**.\nFaites \`${data.prefix}welcome message\` pour configurer le message de bienvenue, \`${data.prefix}welcome channel\` pour configurer le salon de bienvenue et \`${data.prefix}welcome test\` pour tester !`);
+            } else {
+                message.channel.send('⚠️ Le plugin `welcome` est déjà activé.');
             }
 
             break;
         };
         case "goodbye": {
-            if(data.plugins.goodbye.enabled) {
-                data.plugins.goodbye.enabled = false;
-
-                data.markModified("plugins.goodbye.enabled");
-                data.save();
-
-                message.channel.send('✅ Le plugin `goodbye` a bien été **désactivé**.');
-            } else {
+            if(!data.plugins.goodbye.enabled) {
                 data.plugins.goodbye.enabled = true;
 
                 data.markModified("plugins.goodbye.enabled");
                 data.save();
 
-                message.channel.send('✅ Le plugin `goodbye` a bien été **activé**. Faites `' + data.prefix + 'goodbye <message | channel | test>` pour modifier le message et le salon d\'aurevoir!');
+                message.channel.send(`✅ Le plugin \`goodbye\` a bien été **activé**.\nFaites \`${data.prefix}goodbye message\` pour configurer le message d'aurevoir, \`${data.prefix}goodbye channel\` pour configurer le salon d'aurevoir et \`${data.prefix}goodbye test\` pour tester !`);
+            } else {
+                message.channel.send('⚠️ Le plugin `goodbye` est déjà activé.')
             }
 
             break;
         };
         case "logs": {
-            if(data.plugins.logs.enabled) {
-                data.plugins.logs.enabled = false;
-
-                data.markModified("plugins.logs.enabled");
-                data.save();
-
-                message.channel.send('✅ Le plugin `logs` a bien été **désactivé**.');
-            } else {
-                if(!message.guild.me.hasPermission("EMBED_LINKS") || !message.guild.me.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send('J\'ai besoin des permissions Intégrer des liens et Voir les logs du serveur pour pouvoir activer les logs!');
-
+            if(!data.plugins.logs.enabled) {
                 data.plugins.logs.enabled = true;
 
                 data.markModified("plugins.logs.enabled");
                 data.save();
 
-                message.channel.send('✅ Le plugin `logs` a bien été **activé**. Faites `' + data.prefix + 'logschannel` pour modifier le salon de logs!');
+                message.channel.send(`✅ Le plugin \`logs\` a bien été **désactivé**.\nFaites \`${data.prefix}logschannel <channel>\` pour configurer le salon des logs`);
+            } else {
+                message.channel.send('⚠️ Le plugin `logs` est déjà activé.')
             }
 
             break;
         };
         case "autorole": {
-            if(data.plugins.autorole.enabled) {
-                data.plugins.autorole.enabled = false;
-
-                data.markModified("plugins.autorole.enabled");
-                data.save();
-
-                message.channel.send('✅ Le plugin `autorole` a bien été **désactivé**.');
-            } else {
-                if(!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send('J\'ai besoin des permissions Gérer les rôles pour pouvoir activer l\'autorole!');
-
+            if(!data.plugins.autorole.enabled) {
                 data.plugins.autorole.enabled = true;
 
                 data.markModified("plugins.autorole.enabled");
                 data.save();
 
-                message.channel.send('✅ Le plugin `autorole` a bien été **activé**. Faites `' + data.prefix + 'autorole` pour modifier le role!');
+                message.channel.send(`✅ Le plugin \`autorole\` a bien été **activé**.\nFaites \`${data.prefix}autorole <role>\` pour définir un rôle à donner aux nouveaux arrivants !`);
+            } else {
+                message.channel.send('⚠️ Le plugin `autorole` est déjà activé.');
             }
 
             break;
         };
         case "suggestion": {
-            if(data.plugins.suggestion.enabled) {
-                data.plugins.suggestion.enabled = false;
-
-                data.markModified("plugins.suggestion.enabled");
-                data.save();
-
-                message.channel.send('✅ Le plugin `suggestion` a bien été **désactivé**.');
-            } else {
+            if(!data.plugins.suggestion.enabled) {
                 data.plugins.suggestion.enabled = true;
 
                 data.markModified("plugins.suggestion.enabled");
                 data.save();
 
-                message.channel.send('✅ Le plugin `suggestion` a bien été **activé**. Faites `' + data.prefix + 'suggestion-channel` pour modifier le salon!');
+                message.channel.send(`✅ Le plugin \`suggestion\` a bien été **activé**.\nFaites \`${data.prefix}suggestion-channel <channel>\` pour définir le salon des suggestions !`);
+            } else {
+                message.channel.send('⚠️ Le plugin `suggestion` est déjà activé.');
+            }
+
+            break;
+        }
+        case "levels": {
+            if(!data.plugins.levels.enabled) {
+                data.plugins.levels.enabled = true;
+
+                data.markModified("plugins.levels.enabled");
+                data.save();
+
+                message.channel.send(`✅ Le plugin \`levels\` a bien été **activé**.\nRetrouvez les commandes de levels dans la catégorie levels !`);
+            } else {
+                message.channel.send('⚠️ Le plugin `levels` est déjà activé.');
+            }
+
+            break;
+        }
+        case "economy": {
+            if(!data.plugins.economy.enabled) {
+                data.plugins.economy.enabled = true;
+
+                data.markModified("plugins.economy.enabled");
+                data.save();
+
+                message.channel.send(`✅ Le plugin \`economy\` a bien été **activé**.\nRetrouvez toutes les commandes d'économie dans la catégorie economy`);
+            } else {
+                message.channel.send('⚠️ Le plugin `economy` est déjà activé.');
             }
 
             break;
         }
         default: {
-            message.channel.send('⚠️ Ce plugin n\'existe pas. Voici la liste des plugins: `welcome`, `goodbye`, `logs`, `autorole`, `suggestion`. \nVous ne trouvez pas ce que vous voulez ? Faites `' + data.prefix + 'config` pour voir les autres configurations. Le module de protection s\'active séparement via les commandes `raidmode`, `antigiverole`, `antiban`, `antilink`.');
+            message.channel.send('⚠️ Ce plugin n\'existe pas. Voici la liste des plugins: `welcome`, `goodbye`, `logs`, `autorole`, `suggestion`, `levels`, `economy`. \nVous ne trouvez pas ce que vous voulez ? Faites `' + data.prefix + 'config` pour voir les autres configurations. Le module de protection s\'active séparement via les commandes `raidmode`, `antigiverole`, `antiban`, `antilink`.');
         }
     }
 }
@@ -116,7 +114,7 @@ module.exports.help = {
     name: "enable",
     aliases: ["enable", "enable-plugin", "enableplugins", "enable-plugins", "enableplugin"],
     category: 'Config',
-    description: "Activer/Désactiver certains plugins",
+    description: "Activer certains plugins",
     usage: "<plugin>",
     cooldown: 5,
     memberPerms: ["MANAGE_GUILD"],
