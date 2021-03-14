e4f080fc34f20f7a9ff8e3543b214b1842c347a3
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-14 19:37:09 +0100

    🎉 V2.0.0

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.exports = mongoose.model("User", new Schema(
+    {
+        _id: Schema.Types.ObjectId,
+        id: String,
+        warns: {
+            type: Array,
+            default: []
+        },
+        afk: {
+            type: Object,
+            default: {
+                is_afk: false,
+                reason: null
+            }
+        },
+        tempmutes: {
+            type: Array,
+            default: []
+        },
+        money: {
+            type: Number,
+            default: 0
+        },
+        bank: {
+            type: Number,
+            default: 0
+        },
+        cooldowns: {
+            type: Object,
+            default: {
+                work: null,
+                rob: null,
+                crime: null,
+                daily: null,
+                weekly: null
+            }
+        },
+        rankcard: {
+            type: Object,
+            default: {
+                progress_bar_color: "#0F7FBB",
+                text_color: "#0F7FBB",
+                avatar_color: "#ffffff"
+            }
+        }
+    }
+))
