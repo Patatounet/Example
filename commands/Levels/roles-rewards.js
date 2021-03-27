f28ecd834a22a02f8ed298b91d06577967d9bbed
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-03-27 21:00:32 +0100

    ➕ Roles rewards are now displayed from lowest to biggest

--- commands/Levels/roles-rewards.js
+++ commands/Levels/roles-rewards.js
@@ -3,13 +3,14 @@ module.exports.run = async (client, message, args, data) => {
 
     if(data.plugins.levels.roles_rewards.length < 1) return message.channel.send(`⚠️ Aucun rôle récompense à afficher. Ajoutez-en avec la commande \`${data.prefix}add-role <role> <niveau>\``);
 
-    // TODO: Afficher les rôles dans l'ordre
-    const allRoles = data.plugins.levels.roles_rewards.map(obj => `**${Object.keys(obj).join().replace(/l/g, 'Niveau ')}** : ${message.guild.roles.cache.get(Object.values(obj)[0]) ? message.guild.roles.cache.get(Object.values(obj)[0]) : `Role supprimé (ID: ${Object.values(obj)[0]})`}`).join("\n");
+    data.plugins.levels.roles_rewards = data.plugins.levels.roles_rewards.sort((a, b) => Object.keys(a).join().replace(/l/g, '') - Object.keys(b).join().replace(/l/g, ''));
+
+    const allRoles = data.plugins.levels.roles_rewards.map(obj => `**${Object.keys(obj).join().replace(/l/g, 'Niveau ')}** : ${message.guild.roles.cache.get(Object.values(obj)[0]) ? message.guild.roles.cache.get(Object.values(obj)[0]) : `Role supprimé (ID: ${Object.values(obj)[0]})`}`).join('\n');
 
     message.channel.send({
         embed: {
             color: client.config.embed.color,
-            title: "Liste des récompenses de rôles sur le serveur",
+            title: 'Liste des récompenses de rôles sur le serveur',
             author: {
                 icon_url: message.author.displayAvatarURL({ dynamic: true }),
                 name: message.author.tag
