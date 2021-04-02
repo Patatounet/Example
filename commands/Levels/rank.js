e189f25bb869e2a3bc1feb1338f2e0285be0f5b3
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-04-02 16:00:21 +0200

    🐛 fix

--- commands/Levels/rank.js
+++ commands/Levels/rank.js
@@ -1,12 +1,12 @@
 module.exports.run = async (client, message, args, data) => {
     if(!data.plugins.levels.enabled) return message.channel.send(`⚠️ Le système de niveau n'est pas activé sur le serveur. Activez-le avec la commande \`${data.prefix}enable levels\``);
 
-	const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(u => u.user.username.toLowerCase().includes(args[0]?.toLowerCase())) || message.guild.member(message.author);
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(u => u.user.username.toLowerCase().includes(args[0]?.toLowerCase())) || message.guild.member(message.author);
 
     const p = data.members.map(m => m.id).indexOf(member.id);
     const userData = data.members[p];
     const dbUser = await client.findOrCreateUser(member.user);
-
+    
     if(p == -1 || !userData) return message.channel.send('⚠️ Cet utilisateur n\'est pas classé.');
     if(!dbUser) return message.channel.send('❌ Oops ! Une erreur est survenue, veuillez réessayer.');
 
