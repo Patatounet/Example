5f61aa400c22846b148d0c6c17edc7047027e971
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-04-02 15:55:08 +0200

    📡 Status optimisation

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
-module.exports = client => {
+module.exports = async (client) => {
     const setActivity = () => setTimeout(async () => {
-        await client.user.setPresence({ activity: { name: client.config.status.name.replace(/{serversCount}/g, client.guilds.cache.size).replace(/{usersCount}/g, client.getAllUsers()), type: client.config.status.type } });
+        await client.user.setPresence({ activity: { name: client.config.status.name.replace(/{serversCount}/g, client.guilds.cache.size).replace(/{usersCount}/g, client.getAllUsers), type: client.config.status.type } });
 
         setActivity();
     }, 30 * 60 * 1000); // 30 min
 
     setActivity();
+
+    await client.user.setPresence({ activity: { name: client.config.status.name.replace(/{serversCount}/g, client.guilds.cache.size).replace(/{usersCount}/g, client.getAllUsers), type: client.config.status.type } });
 
     client.channels.fetch(client.config.support.logs).then(channel => {
         channel.send("✅ **Le bot est connecté!**");
