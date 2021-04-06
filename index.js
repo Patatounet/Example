e762bd14dc2fde78ec0692820b6d49fc05cc95d2
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-04-06 15:47:04 +0200

    🔧 Some improvements

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 if(Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("La version de Node.js est inférieure à la 12.0.0. Veuillez vous mettre en v12.0.0 ou plus.");
 
-const RainsBot = require('./structure/RainsBot');
-const client = new RainsBot();
+const client = new (require('./structure/RainsBot'))();
 const moment = require('moment');
 
 client.init();
