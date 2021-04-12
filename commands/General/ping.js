044950fc129676d165e81eb57056bd7b2d2f9e56
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-04-12 16:08:41 +0200

    👌 Better ping command

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -1,31 +1,6 @@
-const emojis = require('../../emojis');
-const Guild = require('../../models/Guild');
-
 module.exports.run = async (client, message) => {
-    const embed = {
-        color: client.config.embed.color,
-        fields: [
-            { name: '**Latence messages**', value: emojis.chargement },
-            { name: '**Latence API**', value: emojis.chargement },
-            { name: '**Base de données**', value: emojis.chargement },
-        ],
-        footer: {
-            text: client.config.embed.footer,
-            icon_url: client.user.displayAvatarURL()
-        }
-    };
-
-    message.channel.send({ embed: embed }).then(async (m) => {
-        embed.fields[0].value = `${Date.now() - m.createdTimestamp}ms`;
-
-        embed.fields[1].value = `${client.ws.ping}ms`;
-
-        const date = Date.now();
-        await Guild.findOne({ id: message.guild.id });
-
-        embed.fields[2].value = `${Date.now() - date}ms`;
-
-        await m.edit({ embed: embed });
+    message.channel.send('Pinging...').then(async(msg) => {
+        await msg.edit(`Mon ping est de ${Date.now() - msg.createdTimestamp}ms`);
     });
 }
 
