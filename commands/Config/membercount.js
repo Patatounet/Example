3b28b8cd833ccbb08e94e3a2d4be8de92107495e
COCO150 <70056814+COCO150@users.noreply.github.com>
2021-04-12 16:09:40 +0200

    🆕 NEW: membercount command

---
+++ commands/Config/membercount.js
@@ -0,0 +1,149 @@
+module.exports.run = async (client, message, args, data) => {
+    if(args[0].toLowerCase() === 'create') {
+        if(data.plugins.membercount?.parentID && message.guild.channels.resolve(data.plugins.membercount?.parentID)) {
+            return message.channel.send(`⚠️ Les salons compteur de membre existent déjà ! Faites \`${data.prefix}membercount delete\` pour le retirer`);
+        } else {
+            try {
+                if(message.guild.memberCount !== message.guild.members.cache.size) await message.guild.members.fetch();
+
+                const prompts = [
+                    'Quel nom souhaitez vous donner aux salons de membres (humains) ?\nExemple: `👨 Humains: {count}`',
+                    'Quel nom souhaitez vous donner aux salons de membres (bots) ?\nExemple: `🤖 Bots: {count}`',
+                    'Quel nom souhaitez vous donner aux salons de membres (total) ?\nExemple: `🌎 Total: {count}`'
+                ];
+                const filter = (m) => m.author.id === message.author.id;
+
+                const membersChannelText = await message.channel.send(prompts[0]);
+
+                message.channel.awaitMessages(filter, { time: 30000, max: 1 })
+                    .then(async (membersChannelName) => {
+                        if(!membersChannelName.first().content) return message.channel.send('⚠️ Merci de spécifier un nom.');
+                        if(!membersChannelName.first().content.includes('{count}')) return message.channel.send('⚠️ Vous devez mettre la balise de nombre de membre `{count}`. Sans elle, les salons compteurs ne serviraient à rien...');
+
+                        membersChannelName.first().delete().catch(() => {});
+                        membersChannelText.delete().catch(() => {});
+
+                        const botsChannelText = await message.channel.send(prompts[1]);
+
+                        message.channel.awaitMessages(filter, { time: 30000, max: 1 })
+                            .then(async (botsChannelName) => {
+                                if(!botsChannelName.first().content) return message.channel.send('⚠️ Merci de spécifier un nom.');
+                                if(!botsChannelName.first().content.includes('{count}')) return message.channel.send('⚠️ Vous devez mettre la balise de nombre de membre `{count}`. Sans elle, les salons compteurs ne serviraient à rien...');
+    
+                                botsChannelName.first().delete().catch(() => {});
+                                botsChannelText.delete().catch(() => {});
+
+                                const totalChannelText = await message.channel.send(prompts[2]);
+
+                                message.channel.awaitMessages(filter, { time: 30000, max: 1 })
+                                    .then(async (totalChannelName) => {
+                                        if(!totalChannelName.first().content) return message.channel.send('⚠️ Merci de spécifier un nom.');
+                                        if(!totalChannelName.first().content.includes('{count}')) return message.channel.send('⚠️ Vous devez mettre la balise de nombre de membre `{count}`. Sans elle, les salons compteurs ne serviraient à rien...');
+            
+                                        totalChannelName.first().delete().catch(() => {});
+                                        totalChannelText.delete().catch(() => {});
+
+                                        const parent = await message.guild.channels.create('Stats du serveur', {
+                                            type: 'category',
+                                            permissionOverwrites: [
+                                                {
+                                                    id: message.guild.roles.everyone.id,
+                                                    allow: ['VIEW_CHANNEL']
+                                                }
+                                            ],
+                                            position: 0
+                                        });
+
+                                        const membersChannel = await message.guild.channels.create(membersChannelName.first().content.replace('{count}', message.guild.members.cache.filter(m => !m.user.bot).size), {
+                                            type: 'voice',
+                                            parent
+                                        });
+
+                                        const botsChannel = await message.guild.channels.create(botsChannelName.first().content.replace('{count}', message.guild.members.cache.filter(m => m.user.bot).size), {
+                                            type: 'voice',
+                                            parent
+
+                                        });
+
+                                        const totalChannel = await message.guild.channels.create(totalChannelName.first().content.replace('{count}', message.guild.memberCount), {
+                                            type: 'voice',
+                                            parent
+                                        });
+
+                                        data.plugins.membercount = {
+                                            channels: {
+                                                members: {
+                                                    name: membersChannelName.first().content,
+                                                    id: membersChannel.id
+                                                },
+                                                bots: {
+                                                    name: botsChannelName.first().content,
+                                                    id: botsChannel.id
+                                                },
+                                                totalMembers: {
+                                                    name: totalChannelName.first().content,
+                                                    id: totalChannel.id
+                                                }
+                                            },
+                                            parentID: parent.id
+                                        };
+                                        data.markModified("plugins.membercount");
+
+                                        await data.save();
+
+                                        message.channel.send('✅ Les salons de compteur de membre ont bien été mis en place.');
+                                    })
+                                    .catch(() => message.channel.send('Temps écoulé'));
+                            })
+                            .catch(() => message.channel.send('Temps écoulé'));
+                    })
+                    .catch(() => message.channel.send('Temps écoulé'));
+            } catch (e) {
+                console.error(e);
+                client.channels.cache.get(client.config.support.logs).send(`Une erreur est survenue lors de la commande membercount: \n\`\`\`${e.stack}\`\`\``);
+                return message.channel.send(`Une erreur est survenue lors de la création des salons: \n\`\`\`${e.message}\`\`\``);
+            }
+        }
+    } else if(args[0].toLowerCase() === 'delete') {
+        if(!data.plugins.membercount?.parentID || !message.guild.channels.resolve(data.plugins.membercount?.parentID)) return message.channel.send(`⚠️ Les salons de compteur de membres ne sont pas activés sur ce serveur, faites \`${data.prefix}membercount create\` pour les créer !`);
+
+        const deleteChannel = (channel) => setTimeout(async () => await message.guild.channels.resolve(channel)?.delete().catch(() => {}), 1000);
+
+        deleteChannel(data.plugins.membercount.parentID);
+        Object.values(data.plugins.membercount.channels).forEach((channel) => deleteChannel(channel.id));
+
+        data.plugins.membercount = {
+            channels: {
+                members: {
+                    name: null,
+                    id: null
+                },
+                bots: {
+                    name: null,
+                    id: null
+                },
+                totalMembers: {
+                    name: null,
+                    id: null
+                }
+            },
+            parentID: null
+        };
+        data.markModified("plugins.membercount");
+
+        await data.save();
+        setTimeout(() => message.channel.send('✅ Les salons de compteur de membres ont été supprimés'), 1500);
+    } else message.channel.send(`⚠️ Vous n'utilisez pas la commande correctement.\nFaites \`${data.prefix}membercount create\` pour créer le compteur de membre, et \`${data.prefix}membercount delete\` pour le supprimer.`);
+}
+
+module.exports.help = {
+    name: "membercount",
+    aliases: ["membercount", "counter", "member-count"],
+    category: 'Config',
+    description: "Configurer le salon de compteur de membres.",
+    usage: "<create | delete>",
+    cooldown: 10,
+    memberPerms: ["MANAGE_CHANNELS"],
+    botPerms: ["MANAGE_CHANNELS"],
+    args: true
+}
