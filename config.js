6e7bbce993b83c166e2af591d170d84f333c92f0
Patatounet <87875788+Patatounet@users.noreply.github.com>
2021-10-08 20:19:22 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 module.exports = {
     support: {
-        id: "",
-        logs: "",
-        votes: "",
-        voteRole: ""
+        id: "852221021875732500",
+        logs: "852316106898407445",
+        votes: "852316106898407445",
+        voteRole: "869977602658533436"
     },
-    prefix: "?",
+    prefix: "s!",
     defaultsSettings: {
         lang: "fr",
         welcomeMessage: "Bienvenue {user} sur le serveur **{guildName}** !",
         goodbyeMessage: "Aurevoir {user}, nous espérons te revoir bientôt !"
     },
     embed: {
-        color: "#4696d1",
-        footer: "RainsBot - Open Source"
+        color: "#000000",
+        footer: "Straw - .gg/straw"
     },
     owner: {
         id: "383667743037194240",
-        name: "COCO150#0001"
+        name: "Patatounet#0001"
     },
     status: {
         name: "{serversCount} servers | {usersCount} users",
-        type: "COMPETING"
+        type: "WATCHING"
     }
 }
